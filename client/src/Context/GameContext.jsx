18763d16598af5610763fcb192560dbdf8b0d4d1
TerryNguyen1403 <giangnht1403@gmail.com>
2025-11-20 15:42:56 +0700

    refactor: GameContext.jsx

--- client/src/Context/GameContext.jsx
+++ client/src/Context/GameContext.jsx
@@ -1,17 +1,16 @@
 import { createContext, useEffect, useState } from "react";
-import axios from "axios";
+import api from "../api/axios";
 
 // eslint-disable-next-line react-refresh/only-export-components
 export const GameContext = createContext(null);
 
 const GameContextProvider = (props) => {
   const [games, setGames] = useState([]);
-  // const [isAuthenticated, setIsAuthenticated] = useState(!!localStorage.getItem('token'));
 
   useEffect(() => {
     const fetchGames = async () => {
       try {
-        const response = await axios.get("http://localhost:3000/api/game/all");
+        const response = await api.get("/game/all");
         setGames(response.data);
       } catch (error) {
         console.error("Xảy ra lỗi khi fetch sản phẩm: ", error);
