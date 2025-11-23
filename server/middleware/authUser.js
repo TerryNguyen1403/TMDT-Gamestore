f662e9a619f9ebf304500e69135314bd454099b0
TerryNguyen1403 <giangnht1403@gmail.com>
2025-11-23 12:35:13 +0700

    Admin authorization

--- server/middleware/authUser.js
+++ server/middleware/authUser.js
@@ -1,5 +1,6 @@
 import jwt from "jsonwebtoken";
 import "dotenv/config";
+import User from "../model/User.js";
 
 export const verify = async (req, res, next) => {
   // Lấy JWT từ headers
@@ -20,5 +21,30 @@ export const verify = async (req, res, next) => {
     next();
   } catch (error) {
     return res.status(401).json({ message: "Token không hợp lệ!" });
+  }
+};
+
+// Chỉ cho phép admin truy cập
+export const adminOnly = async (req, res, next) => {
+  try {
+    const userId = req?.user?.userId;
+    if (!userId) {
+      return;
+    }
+
+    const user = await User.findById(userId).select("isAdmin");
+    if (!user) {
+      return;
+    }
+
+    if (!user.isAdmin) {
+      return res
+        .status(403)
+        .json({ message: "Không có quyền truy cập (Admin)" });
+    }
+
+    next();
+  } catch (error) {
+    return res.status(500).json({ message: "Lỗi xác thực quyền admin" });
   }
 };
